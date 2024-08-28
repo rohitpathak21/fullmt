b8378ef85cd4ab5df8af4625720cd3ffc4615769
rohitpathak21 <112495121+rohitpathak21@users.noreply.github.com>
2024-08-28 22:21:37 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -76,6 +76,8 @@ export const login = async (req, res) => {
     res.cookie("token", token , {
       httpOnly: true,
       maxAge: age,
+      sameSite: 'None',
+      secure: true,
     }).status(200).json(userInfo);
 
   }
