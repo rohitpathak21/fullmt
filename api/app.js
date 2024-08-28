302ac699b4323c2bdb5471a13a643e5af5247f98
rohitpathak21 <112495121+rohitpathak21@users.noreply.github.com>
2024-08-28 13:59:32 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
